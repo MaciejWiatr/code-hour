89e39cce8ad3ee6edc22b89ca54a0305d95c25e5
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-04-16 20:21:37 +0200

    History section refactor

--- src/Components/History/HistorySection.tsx
+++ src/Components/History/HistorySection.tsx
@@ -1,8 +1,34 @@
 import { Flex, Box, Text, Link } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { FiExternalLink } from "react-icons/fi";
 import useTwitter from "../../hooks/useTwitter";
 import { ITweetHistoryElement } from "../../ts/interfaces";
+
+interface IHistoryListElementProps {
+	tw: ITweetHistoryElement;
+}
+
+const HistoryListElement: FC<IHistoryListElementProps> = ({ tw }) => {
+	return (
+		<Box
+			w="full"
+			h="28"
+			bg="gray.700"
+			borderRadius="5px"
+			p="2"
+			mb="4"
+			color="gray.400"
+			fontSize="sm"
+		>
+			<Text>{tw.text}</Text>
+			<Link href={tw.link} isExternal color="red.400">
+				<Flex alignItems="center">
+					<Text mr="1">Go to</Text> <FiExternalLink />
+				</Flex>
+			</Link>
+		</Box>
+	);
+};
 
 function HistorySection() {
 	const { getHistory } = useTwitter();
@@ -25,7 +51,6 @@ function HistorySection() {
 			<Text
 				fontWeight="medium"
 				fontSize="xl"
-				// mb="2"
 				color="gray.300"
 				position="sticky"
 				top="0"
@@ -37,24 +62,10 @@ function HistorySection() {
 			<Box as="ul" maxH="435px" borderRadius="5px" overflowY="scroll">
 				{tweets.map((tw, index) => {
 					return (
-						<Box
+						<HistoryListElement
 							key={index}
-							w="full"
-							h="28"
-							bg="gray.700"
-							borderRadius="5px"
-							p="2"
-							mb="4"
-							color="gray.400"
-							fontSize="sm"
-						>
-							<Text>{tw.text}</Text>
-							<Link href={tw.link} isExternal color="red.400">
-								<Flex alignItems="center">
-									<Text mr="1">Go to</Text> <FiExternalLink />
-								</Flex>
-							</Link>
-						</Box>
+							tw={tw}
+						></HistoryListElement>
 					);
 				})}
 			</Box>
